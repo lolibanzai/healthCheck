44b478e15f41b9c60c4197e004d386d09f1fbf4f
rbinghung <pklaz0078@gmail.com>
2017-10-17 14:51:07 +0800

    888

--- test/test/Controllers/DefaultController.cs
+++ test/test/Controllers/DefaultController.cs
@@ -11,7 +11,7 @@ namespace test.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            int lolibanai = 7;
+            int lolibanai = 8;
             return Content(lolibanai.ToString());
         }
 
