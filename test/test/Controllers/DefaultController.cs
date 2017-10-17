661ba5b19cd1ba6fc526eb59e73d26ea2a32220a
江桓峰 <q052293021@gmail.com>
2017-10-17 14:33:31 +0800

    lolibanzai=5

--- test/test/Controllers/DefaultController.cs
+++ test/test/Controllers/DefaultController.cs
@@ -11,7 +11,7 @@ namespace test.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            int lolibanai = 4;
+            int lolibanai = 5;
             return Content(lolibanai.ToString());
         }
 
